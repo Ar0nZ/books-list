90547bfa0729d8042b24eb6924bd47646e7b2309
Aron Zych <aron.zych@1connect.email>
2023-12-10 20:02:32 +0100

    feat: add nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AddBookComponent } from './ui/add-book/add-book.component';
 import { ListBooksComponent } from './ui/list-books/list-books.component';
+import { NavComponent } from './ui/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, RouterOutlet, AddBookComponent, ListBooksComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    AddBookComponent,
+    ListBooksComponent,
+    NavComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
