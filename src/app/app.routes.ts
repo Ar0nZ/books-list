e48b7af741b32469adea785a12c142706efc9e52
Aron Zych <aron.zych@1connect.email>
2023-12-10 20:48:06 +0100

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListBooksComponent } from './ui/list-books/list-books.component';
+import { AddBookComponent } from './ui/add-book/add-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'books', component: ListBooksComponent },
+  { path: 'add-book', component: AddBookComponent },
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+];
--- src/app/ui/nav/nav.component.ts
+++ src/app/ui/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
